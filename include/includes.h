6f20953c6118bf13542acded70ae957abd5bc66e
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2023-01-28 18:10:50 +0100

    added include header

---
+++ include/includes.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 14:45:31 by fsandel           #+#    #+#             */
+/*   Updated: 2023/01/28 18:10:12 by fsandel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDES_H
+# define INCLUDES_H
+
+
+# include "../lib/readline/history.h"
+# include "../lib/readline/readline.h"
+
+
+# ifndef LIBFT_H
+#  include "../lib/libft/libft.h"
+# endif
+
+#endif
