7f52f9f50606d7aeed673c6911e0d0c864b28668
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2023-01-30 16:18:33 +0100

    include header

--- include/includes.h
+++ include/includes.h
@@ -6,15 +6,26 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 14:45:31 by fsandel           #+#    #+#             */
-/*   Updated: 2023/01/29 12:35:13 by fsandel          ###   ########.fr       */
+/*   Updated: 2023/01/30 16:16:56 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDES_H
 # define INCLUDES_H
 
-# include <stdlib.h>
-# include <unistd.h>
+# include <stdlib.h> // malloc, free, exit, getenv
+# include <unistd.h> //write, access, read, close, fork, getcwd, chdir, unlink
+						//execve, dup, dup2, pipe, isatty, ttyname, ttyslot
+# include <stdio.h> //printf, strerror, perror
+# include <fcntl.h> //open
+# include <sys/wait.h> //wait, waitpid, wait3, wait4
+# include <signal.h> //signal, sigaction, sigemptyset, sigaddset, kill
+# include <sys/stat.h> //stat, lstat, fstat
+# include <dirent.h> //opendir, readdir, closedir
+# include <sys/ioctl.h> //ioctl
+# include <termios.h> //tcsetattr tcgetattr
+# include <curses.h> // tgetent, tgetflag tgetstr, tgoto, tpus
+# include <term.h>
 
 # include "../src/executor/executor.h"
 # include "../src/parser/parser.h"
@@ -29,7 +40,10 @@
 # include "macros.h"
 
 # include "../lib/libft/libft.h"
-//# include "../lib/readline/history.h"
-//# include "../lib/readline/readline.h"
+# include "readline/readline.h"
+# include "readline/history.h"
+
+//#include <readline/readline.h>
+//#include <readline/history.h>
 
 #endif
