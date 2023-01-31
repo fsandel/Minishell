6e2bb227708cedd213d7c9937f6985b40fe78c5c
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2023-01-31 13:58:33 +0100

    added input check

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 17:08:34 by fsandel           #+#    #+#             */
-/*   Updated: 2023/01/30 17:16:33 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/01/31 13:57:51 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,35 @@ char	*get_prompt(void)
 	return (prompt);
 }
 
+int	check_input(char *str)
+{
+	int	esc;
+	int	sgl;
+	int	dbl;
+	int	i;
+
+	esc = 0;
+	sgl = 0;
+	dbl = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' && !dbl)
+			sgl = (sgl + 1) % 2;
+		if (str[i] == '\\')
+			esc = (esc + 1) % 2;
+		if (str[i] == '\"' && !esc && !sgl)
+			dbl = (dbl + 1) % 2;
+		if (str[i] && str[i] != '\\')
+			esc = 0;
+		i++;
+	}
+	if (sgl || dbl)
+		return (-1);
+	else
+		return (0);
+}
+
 int	main(void)
 {
 	char	*input;
@@ -33,9 +62,11 @@ int	main(void)
 	while (1)
 	{
 		input = readline(prompt);
+		add_history(input);
 		if (ft_strncmp(input, "exit", 5) == 0)
 			break ;
-		lexing(input);
+		check_input(input);
+		//lexing(input);
 		//ft_printf("%s\n", input);
 		free(input);
 	}
