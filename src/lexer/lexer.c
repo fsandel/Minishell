65f8fa3b867d1436606908133074398a9a83e5bb
pgorner <pgorner@student.42heilbronn.de>
2023-02-11 15:05:53 +0100

    lex v3 working

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 16:41:27 by pgorner           #+#    #+#             */
-/*   Updated: 2023/02/10 19:38:27 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/02/11 14:37:10 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	assign_checks(t_lx *lex)
 	lex->i = 0;
 	lex->te = -2;
 	lex->ts = 0;
-	lex->q = 0;
+	lex->q = 1;
 	lex->quot = ft_strdup("\'\"");
 	lex->pird = ft_strdup("|><");
 	lex->space = ft_strdup(" \t\v\f\r");
@@ -51,7 +51,7 @@ void	token(t_lx *lex, char *input, t_list *tokens)
 	char *token;
 
 	i = 0;
-	lex->te = lex->i++;
+	lex->te = lex->i;
 	token = ft_calloc(sizeof(char), lex->te - lex->ts + 2);
 	if (lex->ts == lex->i)
 		token[i] = input[lex->i];
@@ -72,15 +72,15 @@ void	token(t_lx *lex, char *input, t_list *tokens)
 
 int	check(char input, char const *it)
 {
-	int i;
+	int c;
 
-	i = 0;
-	while(it[i])
+	c = 0;
+	while(it[c])
 	{
 		//printf("COMPARING :%c: & :%c:\n", input, it[i]);
-		if (it[i] == input)
+		if (it[c] == input)
 			return (TRUE);
-		i++;
+		c++;
 	}
 	return (FALSE);
 }
@@ -89,10 +89,11 @@ int	check_null(t_lx *lex, char *input, t_list *tokens)
 {
 	if (input[lex->i] == '\0')
 	{
-		if (lex->i - 1 != lex->te)
+		if (lex->i - 1 != lex->te
+		&& (is_whitespace(input[lex->i - 1]) == FALSE))
 		{
-			//printf("--:%i != %i:--\n", lex->i, lex->te);
-			//printf("token made in null\n");
+			printf("--:%i != %i:--\n", lex->i - 1, lex->te);
+			printf("token made in null\n");
 			//printf("te:%ii:%its%i\n", lex->te, lex->i, lex->ts);
 			token(lex, input, tokens);
 		}
@@ -131,9 +132,13 @@ void doub(t_lx *lex, char *input, t_list *tokens)
 		}
 		else
 		{
+			if (lex->ts != lex->i)
+			{
 			lex->i--;
 			token(lex, input, tokens);
-			lex->i += 2;
+			lex->i++;
+			}
+			lex->i++;;
 			token(lex, input, tokens);
 		}
 	}
@@ -161,9 +166,9 @@ void and(t_lx *lex, char *input, t_list *tokens)
 
 void check_pird(t_lx *lex, char *input, t_list *tokens)
 {
-	if (check(input[lex->i], "|><") == TRUE
-		&& lex->q == FALSE)
+	if (check(input[lex->i], "|><") == TRUE)
 	{
+		lex->q--;
 		//printf("inpird at %i with %c\n", lex->i, input[lex->i]);
 		if (lex->te != lex->i 
 			&& (is_whitespace(input[lex->i - 1]) == FALSE))
@@ -171,11 +176,13 @@ void check_pird(t_lx *lex, char *input, t_list *tokens)
 			lex->i--;
 			//printf("previous token made in pird\n");
 			token(lex, input, tokens);
+			lex->i++;
 		}
 		if (input[lex->i + 1] == input[lex->i])
 			lex->i++;
 		//printf("token made in pird with %i:%c:\n", lex->i, input[lex->i]);
 		token(lex, input, tokens);
+		lex->i++;
 	}
 	if (input[lex->i] == '2')
 		doub(lex, input, tokens);
@@ -187,29 +194,31 @@ void	check_quote(t_lx *lex, char *input, t_list *tokens)
 {
 	if (check(input[lex->i], "\'\"") == TRUE)
 	{
+		lex->q--;
 		lex->c = input[lex->i];
-		//printf("QUOTE?\n");
 		if (lex->te != lex->i - 1
-			&& lex->i != 0)
+			&& lex->i != 0
+			&&is_whitespace(input[lex->i - 1]) == FALSE)
 		{
 			lex->i--;
 			token(lex, input, tokens);
 			lex->i++;
 		}
+		lex->i++;
 		while (input[lex->i])
 		{
-			lex->i++;
 			if (input[lex->i] == '\"'
-				&& input[lex->i - 1] == '\\')
+				&& input[lex->i - 1] == '\\') //escapecounter
 				lex->i++;
 			if (input[lex->i] == lex->c)
 				break;
+			lex->i++;
 		}
 		while (input[lex->i]
-				&&is_whitespace(input[lex->i]) == FALSE
-				&& check(input[lex->i], "|><") == FALSE)
+				&&is_whitespace(input[lex->i + 1]) == FALSE
+				&& check(input[lex->i], "|><") == FALSE
+				&& check(input[lex->i], "\'\"") == FALSE)
 				lex->i++;
-		//printf("token made in quote\n");
 		token(lex, input, tokens);
 		lex->i++;
 	}
@@ -219,6 +228,7 @@ void	check_dollar(t_lx *lex, char *input, t_list *tokens)
 {
 	if (input[lex->i] == '$')
 	{
+		lex->q--;
 		printf("I found a dollar\n");
 		lex->i++;
 	}
@@ -254,6 +264,7 @@ void	check_space(t_lx *lex, char *input, t_list *tokens)
 	if (is_whitespace(input[lex->i]) == TRUE)
 		//printf("SPAACE at %i\n", lex->i);
 		{
+		lex->q--;
 		if (lex->te != lex->i - 1
 			&& (is_whitespace(input[lex->i - 1]) == FALSE))
 		{
@@ -269,9 +280,10 @@ void	check_space(t_lx *lex, char *input, t_list *tokens)
 
 void	check_left(t_lx *lex, char *input, t_list *tokens)
 {
-	if (input[lex->i]
-		&& lex->q == FALSE)
+	if (lex->q > 0)
 		lex->i++;
+	else
+		lex->q = 1;
 }
 
 t_list	*lexer(char *input)
@@ -301,7 +313,8 @@ t_list	*lexer(char *input)
 			break; */
 		i++;
 	}
-	//ft_lstprint(tokens);
-	ft_lstclear(&tokens, free);
+	printf("--------\n");
+	ft_lstprint(tokens);
+	//ft_lstclear(&tokens, free);
 	return(tokens);
 }
