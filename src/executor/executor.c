23126481b0fd5b3aa24f23378901baa5d88d4414
Florian Sandel <fsandel@student.42heilbronn.de>
2023-02-24 14:43:16 +0100

    added filename expand

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 12:01:38 by fsandel           #+#    #+#             */
-/*   Updated: 2023/02/22 16:34:15 by fsandel          ###   ########.fr       */
+/*   Updated: 2023/02/24 14:39:00 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,12 @@ static void	execute(t_pars **pars, int i)
 		if (pars[i]->error == 0)
 			execve(command, &pars[i]->cmd[0], pars[i]->env);
 		else
+		{
+			free(command);
+			free_array(pars[0]->env);
+			free_struct(pars);
 			exit(1);
+		}
 		g_error = execve_error(command, pars[i]->cmd[0]);
 		free(command);
 	}
--- src/parser/redirect.c
+++ src/parser/redirect.c
@@ -6,7 +6,7 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:12:40 by fsandel           #+#    #+#             */
-/*   Updated: 2023/02/18 10:26:36 by fsandel          ###   ########.fr       */
+/*   Updated: 2023/02/24 14:41:38 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,29 @@ int	unexpected_token(char *str)
 	return (0);
 }
 
+char	*expand_string(char **env, char *str)
+{
+	t_pars	**expand;
+	char	*exp;
+
+	expand = ft_calloc(1, sizeof(t_pars *));
+	expand[0] = setup_single_pars(env, NULL);
+	expand[0]->cmd = ft_calloc(2, sizeof(char *));
+	expand[0]->cmd[0] = ft_strdup(str);
+	expand[0]->cmd[1] = NULL;
+	expand[0]->amount = 1;
+	expand[0]->index = 0;
+	expand[0]->total_cmd = 1;
+	expand = expander(expand);
+	exp = ft_strdup(expand[0]->cmd[0]);
+	free_struct(expand);
+	return (exp);
+}
+
 t_list	*append_out(t_list *list, t_pars *pars)
 {
-	int	fd;
+	int		fd;
+	char	*exp;
 
 	if (!list->next)
 		return (ft_putendl_fd("minishell: syntax error near unexpected token \
@@ -41,19 +61,25 @@ t_list	*append_out(t_list *list, t_pars *pars)
 		return (ft_putstr_fd("minishell: syntax error near unexpected token ",
 				2), ft_putendl_fd(list->next->content, 2), g_error = 258,
 			pars->error = 1, NULL);
-	fd = open(list->next->content, O_WRONLY | O_APPEND, 0644);
+	exp = expand_string(pars->env, list->next->content);
+	if (!ft_strncmp(exp, "", 2))
+		return (ft_err_print("minishell: %s: ambigious redirect",
+				list->next->content, NULL, NULL), g_error = 1, pars->error = 1,
+			free(exp), list->next);
+	fd = open(exp, O_WRONLY | O_APPEND, 0644);
 	if (fd < 0)
-		return (perror(list->next->content), g_error = 1, pars->error = 1,
+		return (free(exp), perror(exp), g_error = 1, pars->error = 1,
 			list->next);
 	if (pars->out > 2)
 		close(pars->out);
 	pars->out = fd;
-	return (list->next);
+	return (free(exp), list->next);
 }
 
 t_list	*redirect_in(t_list *list, t_pars *pars)
 {
-	int	fd;
+	int		fd;
+	char	*exp;
 
 	if (!list->next)
 		return (ft_putendl_fd("minishell: syntax error near unexpected token \
@@ -65,19 +91,25 @@ t_list	*redirect_in(t_list *list, t_pars *pars)
 		return (ft_putstr_fd("minishell: syntax error near unexpected token ",
 				2), ft_putendl_fd(list->next->content, 2), g_error = 258,
 			pars->error = 1, NULL);
-	fd = open(list->next->content, O_RDONLY, 0644);
+	exp = expand_string(pars->env, list->next->content);
+	if (!ft_strncmp(exp, "", 2))
+		return (ft_err_print("minishell: %s: ambigious redirect",
+				list->next->content, NULL, NULL), g_error = 1, pars->error = 1,
+			free(exp), list->next);
+	fd = open(exp, O_RDONLY, 0644);
 	if (fd < 0)
-		return (perror(list->next->content), g_error = 1, pars->error = 1,
-			list->next);
+		return (free(exp), perror(exp), g_error = 1,
+			pars->error = 1, list->next);
 	if (pars->in > 2)
 		close(pars->in);
 	pars->in = fd;
-	return (list->next);
+	return (free(exp), list->next);
 }
 
 t_list	*redirect_out(t_list *list, t_pars *pars)
 {
-	int	fd;
+	int		fd;
+	char	*exp;
 
 	if (!list->next)
 		return (ft_putendl_fd("minishell: syntax error near unexpected token \
@@ -89,12 +121,17 @@ t_list	*redirect_out(t_list *list, t_pars *pars)
 		return (ft_putstr_fd("minishell: syntax error near unexpected token ",
 				2), ft_putendl_fd(list->next->content, 2), g_error = 258,
 			pars->error = 1, NULL);
-	fd = open(list->next->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	exp = expand_string(pars->env, list->next->content);
+	if (!ft_strncmp(exp, "", 2))
+		return (ft_err_print("minishell: %s: ambigious redirect",
+				list->next->content, NULL, NULL), g_error = 1, pars->error = 1,
+			free(exp), list->next);
+	fd = open(exp, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
-		return (perror(list->next->content), g_error = 1, pars->error = 1,
+		return (free(exp), perror(exp), g_error = 1, pars->error = 1,
 			list->next);
 	if (pars->out > 2)
 		close(pars->out);
 	pars->out = fd;
-	return (list->next);
+	return (free(exp), list->next);
 }
